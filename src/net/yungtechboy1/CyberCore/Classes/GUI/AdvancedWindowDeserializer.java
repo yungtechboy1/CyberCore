9001576a2d302988bbadfa72e66ef2db85ed6ddf
yungtechboy1 <yungtechboy1@gmail.com>
2019-03-16 19:42:11 -0500

    DOne!

--- src/net/yungtechboy1/CyberCore/Classes/GUI/AdvancedWindowDeserializer.java
+++ src/net/yungtechboy1/CyberCore/Classes/GUI/AdvancedWindowDeserializer.java
@@ -1,12 +1,11 @@
 package net.yungtechboy1.CyberCore.Classes.GUI;
 
-import cn.nukkit.form.element.Element;
-import cn.nukkit.form.element.ElementButton;
-import cn.nukkit.form.element.ElementStepSlider;
+import cn.nukkit.form.element.*;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AdvancedWindowDeserializer implements JsonDeserializer<AdvancedWindow> {
 
@@ -37,13 +36,57 @@ public class AdvancedWindowDeserializer implements JsonDeserializer<AdvancedWind
         jaction = jo.get("action").getAsString();
 
         switch (jt.toLowerCase()) {
-            case "button":
-                return new ElementButton(jid);
-            break;
-            case "button-img":
-                return new ElementButton(jid);
-            break;
+            case "dropdown":
+                /**
+                 * {
+                 *      default: 0
+                 *      values: Array String
+                 * }
+                 */
+                ElementDropdown ed = new ElementDropdown(jid);
+                Integer jdef = 0;
+                if (jo.has("default")) jdef = jo.get("default").getAsInt();
+                JsonArray jd = jo.getAsJsonArray("values");
+                ArrayList<String> jl = new ArrayList<>();
+                for (int i = 0; i < jd.size(); i++) {
+                    ed.addOption(jd.get(i).getAsString());
+                }
+                ed.setDefaultOptionIndex(jdef);
+                return ed;
+            case "Input":
+                ElementInput ei = new ElementInput(jid);
+                String jtext = jo.get("text").getAsString();
+                String jplaceholder = jo.get("placeholder").getAsString();
+                String jdefault = jo.get("default").getAsString();
+                ei.setText(jtext);
+                ei.setPlaceHolder(jplaceholder);
+                ei.setDefaultText(jdefault);
+                return ei;
+            default:
+                return null;
         }
 
+    }
+
+    /**
+     * @param jo
+     * @return ElementButton | ElementButtonImageData
+     */
+    public Object PharseButtonElement(JsonObject jo) {
+        String jt, jid, jimg, jaction = null;
+        jt = jo.get("type").getAsString();
+        jid = jo.get("id").getAsString();
+        jimg = jo.get("img").getAsString();
+        jaction = jo.get("action").getAsString();
+
+        switch (jt.toLowerCase()) {
+            case "button":
+                return new ElementButton(jid);
+            case "button-img":
+                String jurl = jo.get("action").getAsString();
+                return new ElementButtonImageData(jid, jurl);
+            default:
+                return null;
+        }
     }
 }
