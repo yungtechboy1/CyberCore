bd085ed61945d59de538621fa85e0a50ef6b7b2f
yungtechboy1 <yungtechboy1@gmail.com>
2019-03-16 20:23:33 -0500

    DONE! For now.....

--- src/net/yungtechboy1/CyberCore/Classes/GUI/AdvancedWindowDeserializer.java
+++ src/net/yungtechboy1/CyberCore/Classes/GUI/AdvancedWindowDeserializer.java
@@ -23,7 +23,7 @@ public class AdvancedWindowDeserializer implements JsonDeserializer<AdvancedWind
             eeee.add(PharseElement(jo));
         }
 //            for (int x = -size; x < size; x++) {
-        new AdvancedWindow(t, e);
+        new AdvancedWindow(t, eeee);
 
         return null;
     }
@@ -34,7 +34,7 @@ public class AdvancedWindowDeserializer implements JsonDeserializer<AdvancedWind
         jid = jo.get("id").getAsString();
         jimg = jo.get("img").getAsString();
         jaction = jo.get("action").getAsString();
-
+        Object jdefault;
         switch (jt.toLowerCase()) {
             case "dropdown":
                 /**
@@ -44,24 +44,48 @@ public class AdvancedWindowDeserializer implements JsonDeserializer<AdvancedWind
                  * }
                  */
                 ElementDropdown ed = new ElementDropdown(jid);
-                Integer jdef = 0;
-                if (jo.has("default")) jdef = jo.get("default").getAsInt();
+                jdefault = 0;
+                if (jo.has("default")) jdefault = jo.get("default").getAsInt();
                 JsonArray jd = jo.getAsJsonArray("values");
                 ArrayList<String> jl = new ArrayList<>();
                 for (int i = 0; i < jd.size(); i++) {
                     ed.addOption(jd.get(i).getAsString());
                 }
-                ed.setDefaultOptionIndex(jdef);
+                ed.setDefaultOptionIndex((int) jdefault);
                 return ed;
             case "Input":
                 ElementInput ei = new ElementInput(jid);
                 String jtext = jo.get("text").getAsString();
                 String jplaceholder = jo.get("placeholder").getAsString();
-                String jdefault = jo.get("default").getAsString();
+                jdefault = jo.get("default").getAsString();
                 ei.setText(jtext);
                 ei.setPlaceHolder(jplaceholder);
-                ei.setDefaultText(jdefault);
+                ei.setDefaultText((String) jdefault);
                 return ei;
+            case "Label":
+                ElementLabel el = new ElementLabel(jid);
+                return el;
+            case "Slider":
+                float jmin = jo.get("min").getAsFloat();
+                float jmax = jo.get("max").getAsFloat();
+                int jstep = jo.get("step").getAsInt();
+                jdefault = jo.get("default").getAsFloat();
+                return new ElementSlider(jid, jmin, jmax, jstep, (float) jdefault);
+            case "StepSlider":
+                float jmin1 = jo.get("min").getAsFloat();
+                float jmax1 = jo.get("max").getAsFloat();
+                int jstep1 = jo.get("step").getAsInt();
+                jdefault = jo.get("default").getAsFloat();
+                JsonArray jd1 = jo.getAsJsonArray("values");
+                ElementStepSlider ess = new ElementStepSlider(jid, new ArrayList<>(), (int) jdefault);
+                for (int i = 0; i < jd1.size(); i++) ess.addStep(jd1.get(i).getAsString());
+                return ess;
+            case "Toggle":
+                ElementToggle et = new ElementToggle(jid);
+                jdefault = false;
+                if (jo.has("default")) jdefault = jo.get("default").getAsBoolean();
+                et.setDefaultValue((boolean) jdefault);
+                return et;
             default:
                 return null;
         }
