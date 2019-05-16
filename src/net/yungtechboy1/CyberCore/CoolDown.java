38d511fba4a1eabe5fc05c5fd9a6e122a25c9deb
yungtechboy1 <yungtechboy1@gmail.com>
2019-05-16 14:54:55 -0500

    Loads more with Classes / Ability / Power and moreeeee!

---
+++ src/net/yungtechboy1/CyberCore/CoolDown.java
@@ -0,0 +1,7 @@
+package net.yungtechboy1.CyberCore;
+
+/**
+ * Created by carlt on 5/16/2019.
+ */
+public class CoolDown {
+}
---
+++ src/net/yungtechboy1/CyberCore/Custom/Item/CustomItemTNT.java
@@ -0,0 +1,7 @@
+package net.yungtechboy1.CyberCore.Custom.Item;
+
+/**
+ * Created by carlt on 5/16/2019.
+ */
+public class CustomItemTNT {
+}
