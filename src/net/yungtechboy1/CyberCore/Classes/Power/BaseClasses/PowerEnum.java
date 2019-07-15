7c0edfdd14e8d92db289476853569b13e3751bd8
yungtechboy1 <yungtechboy1@gmail.com>
2019-07-14 23:44:22 -0500

    Small Fix

--- src/net/yungtechboy1/CyberCore/Classes/Power/BaseClasses/PowerEnum.java
+++ src/net/yungtechboy1/CyberCore/Classes/Power/BaseClasses/PowerEnum.java
@@ -1,7 +1,7 @@
 package net.yungtechboy1.CyberCore.Classes.Power.BaseClasses;
 
 public enum PowerEnum {
-    Unknown,MercenaryDoubleTake, MercenaryRegeneration, HolyKnightHeal, TNTAirStrike, MineLife, MinerOreKnowledge, TNTSpecalist, MercenaryBlindingStrike, MercenaryDisarm, DragonJumper, KnightSandShield, DarkKnightPosionousStench, FireStomp, FireBox, DoubleTime;
+    Unknown,MercenaryDoubleTake, MercenaryRegeneration, HolyKnightHeal, TNTAirStrike, MineLife, MinerOreKnowledge, TNTSpecalist, MercenaryBlindingStrike, MercenaryDisarm, DragonJumper, KnightSandShield, DarkKnightPosionousStench, FireStomp, FireBox, DoubleTime, AntidotePower;
     public static PowerEnum fromint(int i){
         if(values().length <i){
             return values()[i];
